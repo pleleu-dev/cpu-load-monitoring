ed35ebf02f0418e937d0502abe24eb442ddb0c68
P. Leleu <pleleu.dev@gmail.com>
2024-04-04 18:21:26 +0200

    Card header style

--- frontend/src/App/index.tsx
+++ frontend/src/App/index.tsx
@@ -1,5 +1,11 @@
+import { GoCpu } from 'react-icons/go'
+import { VscPulse } from 'react-icons/vsc'
+import { FiBarChart2 } from 'react-icons/fi'
+import { RiHeartPulseLine } from 'react-icons/ri'
+
 import layout from './layout.module.css'
 import app from './app.module.css'
+import card from './card.module.css'
 
 function App() {
   return (
@@ -9,16 +15,28 @@ function App() {
       </header>
       <main className={`${layout.main} ${app.main}`}>
         <section className={layout.current}>
-          <h2>Current average load</h2>
+          <header className={card.header}>
+            <h2>Current average load</h2>
+            <GoCpu size={36} />
+          </header>
         </section>
         <section className={layout.average}>
-          <h2>Average load</h2>
+          <header className={card.header}>
+            <h2>Average load</h2>
+            <VscPulse size={36} />
+          </header>
         </section>
         <section className={layout.recovery}>
-          <h2>Recovery summary</h2>
+          <header className={card.header}>
+            <h2>Recovery summary</h2>
+            <RiHeartPulseLine size={36} />
+          </header>
         </section>
         <section className={layout.heavy}>
-          <h2>CPU heavy load summary</h2>
+          <header className={card.header}>
+            <h2>CPU heavy load summary</h2>
+            <FiBarChart2 size={36} />
+          </header>
         </section>
       </main>
     </>
